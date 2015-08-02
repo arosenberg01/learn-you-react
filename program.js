d53eb2e419c230dc40b673d2a02f55d00b51f9a3
Ansel Rosenberg <ansel01@gmail.com>
2015-08-01 19:04:31 -0700

    Set up express server w/ react views

---
+++ program.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var reactViews = require('express-react-views');
+require('node-jsx').install();
+
+app.set('port', (process.argv[2] || 3000));
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views');
+app.engine('jsx', reactViews.createEngine());
+
+app.use('/', function(req, res) {
+  res.render('index', '');
+});
+
+app.listen(app.get('port'), function() {});
